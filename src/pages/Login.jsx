8baf2b88a9493bc36fecc11c23b9a9ae58069855
acricmg <127219569+acricmg@users.noreply.github.com>
2023-11-24 00:38:52 +0800

    added styling in login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,40 +3,48 @@ import logo from "../assets/icons/notes-hidemaru.svg";
 
 const Login = () => {
   return (
-    <div className="flex flex-col flex-center mt-24">
-      <div className="flex text-primary">
+    <div className="mt-24 flex flex-col flex-center">
+      <div className="flex flex-center text-primary">
         <img
-          className="h-7 self-center"
+          className="h-10 sm:h-7 self-center"
           src={logo}
           alt="minimalistic icon of sticky notes"
         />
-        <h1 className="font-semibold text-3xl ml-3">AppName</h1>
+        <h1 className="font-semibold text-3xl ml-3 hidden sm:block">AppName</h1>
       </div>
-      <div className="mt-8 flex flex-col">
-        <div>
-          <h3>Log in</h3>
+      <div
+        className="mt-8 flex flex-col border-opacity-80 rounded-xl w-64 sm:w-96 sm:p-9 sm:bg-white sm:border-1 sm:border-gray-300 sm:shadow"
+      >
+        <div className="text-center sm:text-left">
+          <h3 className="font-bold mb-2.5 text-2xl">Log in</h3>
           <p>
-            Don't have an account? <span>Create one</span>
+            Don't have an account?
+            <span className="text-secondary hover:text-secondary-200 hover:font-medium transition ease-out duration-300"> Create one</span>
           </p>
         </div>
         <form className="my-6 flex flex-col" action="" method="post">
           <input
-            className="mb-3 rounded-md"
+            className="mb-3 field-rounded field-border"
             type="email"
             name="email"
             id="email"
             placeholder="Email Address"
           />
           <input
-            className="mb-3 rounded-md"
+            className="mb-3 field-rounded field-border"
             type="password"
             name="password"
             id="passwd"
             placeholder="Password"
           />
-          <button type="submit">Log in</button>
+          <button
+            className="field-rounded bg-primary text-white font-medium hover:bg-primary-200 hover:shadow-md transition ease-out duration-500"
+            type="submit"
+          >
+            Log in
+          </button>
         </form>
-        <span className="self-center">Forgot Password?</span>
+        <span className="text-center text-secondary hover:text-secondary-200 hover:font-medium transition ease-out duration-300">Forgot Password?</span>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         primary: "#407888",
-        secondary: "#ADD8E6",
-      },
-      opacity: {
-        16: "0.16",
+        "primary-100": "#326878",
+        "primary-200": "#265968",
+        "primary-300": "#194653",
+        secondary: "#5097AB",
+        "secondary-100": "#398296",
+        "secondary-200": "#276A7C",
+        "secondary-300": "#184957",
       },
     },
-    
   },
   plugins: [],
-}
-
+};
