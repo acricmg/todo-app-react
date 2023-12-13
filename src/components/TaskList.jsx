8dc53c5e7441d640a57f0ec160d368c2454b6c69
acricmg <127219569+acricmg@users.noreply.github.com>
2023-12-13 22:35:35 +0800

    created handler functions for adding of tasks

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,3 +1,9 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { DndProvider, useDrag, useDrop } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { useNavigate } from "react-router-dom";
+
 import { faCheckCircle, faCircle } from "@fortawesome/free-regular-svg-icons";
 import {
   faCirclePlus,
@@ -7,11 +13,7 @@ import {
   faTrash,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { DndProvider, useDrag, useDrop } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { useNavigate } from "react-router-dom";
+
 import config from "../../config/config";
 import EditPrompt from "./EditPrompt";
 import Notification from "./Notification";
@@ -96,6 +98,7 @@ const TaskList = ({ userId }) => {
   const navigate = useNavigate();
   const [notification, setNotification] = useState(null);
   const [editPrompt, setEditPrompt] = useState(null);
+  const [AddPrompt, setAddPrompt] = useState(null);
   const [editTaskSelected, setEditTaskSelected] = useState(null);
   const [notificationColor, setNotificationColor] = useState("");
 
@@ -127,9 +130,25 @@ const TaskList = ({ userId }) => {
     fetchData();
   };
 
+  const handleAddPromptVisibility = (closed, alert, success) => {
+    setAddPrompt(closed ? null : true);
+    if (alert) {
+      if (success) {
+        showNotification("Task created", "#00b300");
+      } else {
+        showNotification("Task not created", "#b30000");
+      }
+    }
+    fetchData();
+  };
+
   const showEditPrompt = (task) => {
     setEditTaskSelected(task);
     handleEditPromptVisibility(false);
+  };
+
+  const showAddPrompt = () => {
+    handleAddPromptVisibility(false);
   };
 
   const moveItem = (fromIndex, toIndex) => {
@@ -204,127 +223,96 @@ const TaskList = ({ userId }) => {
   }, []); // Empty dependency array ensures the effect runs only once when the component mounts
 
   return (
-    <div className="w-full p-7 flex justify-center gap-8">
+    <div className="w-full p-7">
       {editPrompt && (
         <EditPrompt
           task={editTaskSelected}
           parentOnClose={handleEditPromptVisibility}
         />
       )}
-      <div>
-        {notification && (
-          <Notification
-            message={notification}
-            color={notificationColor}
-            onClose={closeNotification}
-          />
-        )}
-      </div>
-      <div className="h-fit bg-white rounded-lg p-8 drop-shadow-md flex-1 ">
-        <div className="flex items-center border-b-2 pb-4 mb-8">
-          <h2 className="mr-3 text-3xl font-bold">Tasks</h2>
-          <div className="circle-sm bg-primary text-white">
-            <span>5</span>
-          </div>
-        </div>
-        <DndProvider backend={HTML5Backend}>
-          <div>
-            {unfinishedTaskData ? (
-              unfinishedTaskData.map((task, index) => (
-                <DraggableListItem
-                  id={task._id}
-                  task={task}
-                  index={index}
-                  moveItem={moveItem}
-                  handleCircleClick={handleCircleClick}
-                  showEditPrompt={showEditPrompt}
-                />
-              ))
-            ) : taskData && !Object.keys(taskData).includes("unfinished") ? (
-              <div>
-                <p>No tasks. Good Job! 🥳</p>
-              </div>
-            ) : (
-              <p>Loading...</p>
-            )}
-          </div>
-        </DndProvider>
-        <div className="mt-7 flex border-t-2">
-          <div className="flex gap-4">
-            <div className="mt-4 text-primary opacity-60 flex items-center relative cursor-pointer transition duration-300 ease-in-out transform hover:opacity-100">
-              <FontAwesomeIcon icon={faCirclePlus} />
-              <p className="ml-3 font-medium">Add a new task</p>
-              {/* <input
-                className="ml-3 bg-white border-b"
-                id="title"
-                name="title"
-                onChange={handleNewTaskChange}
-                type="text"
-                placeholder="Task Title"
-              /> */}
+      {notification && (
+        <Notification
+          message={notification}
+          color={notificationColor}
+          onClose={closeNotification}
+        />
+      )}
+      <div className="flex justify-center gap-8">
+        <div className="h-fit bg-white rounded-lg p-8 drop-shadow-md flex-1 ">
+          <div className="flex items-center border-b-2 pb-4 mb-8">
+            <h2 className="mr-3 text-3xl font-bold">Tasks</h2>
+            <div className="circle-sm bg-primary text-white">
+              <span>5</span>
             </div>
-            {/* <div className="relative items-center">
-              <textarea
-                id="description"
-                name="description"
-                onChange={handleNewTaskChange}
-                className="mt-5 ml-7 bg-white border w-full rounded py-2"
-                placeholder="New Task description"
-              />
-            </div> */}
-            {/* {newTask.dueDate} */}
           </div>
-          {/* <div className="w-[50px] relative ml-10">
-            {newTask.title != null ? (
-              <button className="absolute right-0 top-0 bottom-0 bg-blue-200 h-1/2 px-3 rounded-lg my-auto h-45">
-                <FontAwesomeIcon
-                  className="text-white"
-                  icon={faFloppyDisk}
-                  onClick={createTask}
-                />
-              </button>
-            ) : (
-              ""
-            )}
-          </div> */}
-        </div>
-      </div>
-
-      <div className="h-fit bg-white rounded-lg p-8 drop-shadow-md flex-1">
-        <div className="flex items-center border-b-2 pb-4 mb-8">
-          <h2 className="mr-3 text-3xl font-bold">Completed</h2>
-          <div className="circle-sm bg-success text-white">
-            <span>2</span>
-          </div>
-        </div>
-        {taskData && taskData["finished"] !== undefined ? (
-          <ul>
-            {taskData["finished"].map((task) => (
-              <li className="mb-5 text-left" key={task._id}>
-                <div className="flex items-center">
-                  <FontAwesomeIcon
-                    icon={
-                      task.status.toLowerCase() == "finished"
-                        ? faCheckCircle
-                        : faCircle
-                    }
-                    onClick={() =>
-                      handleCircleClick(task, "unfinished", "#b30000")
-                    }
-                    className="cursor-pointer transition duration-300 ease-in-out transform hover:opacity-100 opacity-50"
+          <DndProvider backend={HTML5Backend}>
+            <div>
+              {unfinishedTaskData ? (
+                unfinishedTaskData.map((task, index) => (
+                  <DraggableListItem
+                    id={task._id}
+                    task={task}
+                    index={index}
+                    moveItem={moveItem}
+                    handleCircleClick={handleCircleClick}
+                    showEditPrompt={showEditPrompt}
                   />
-                  <p className="font-bold ml-3">{task.title}</p>
+                ))
+              ) : taskData && !Object.keys(taskData).includes("unfinished") ? (
+                <div>
+                  <p>No tasks. Good Job! 🥳</p>
                 </div>
-                <p className="ml-7 text-slate-500">{task.description}</p>
-                <p className="ml-7 text-slate-500">{task.status}</p>
-              </li>
-            ))}
-          </ul>
-        ) : taskData && !Object.keys(taskData).includes("Finished") ? (
-          <p>bro too lazy 😭</p>
-        ) : (
-          <p>Loading...</p>
-        )}
+              ) : (
+                <p>Loading...</p>
+              )}
+            </div>
+          </DndProvider>
+          <div className="mt-7 flex border-t-2">
+            <div className="flex gap-4">
+              <div className="mt-4 text-primary opacity-60 flex items-center relative cursor-pointer transition duration-300 ease-in-out transform hover:opacity-100">
+                <FontAwesomeIcon icon={faCirclePlus} />
+                <p className="ml-3 font-medium">Add a new task</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="h-fit bg-white rounded-lg p-8 drop-shadow-md flex-1">
+          <div className="flex items-center border-b-2 pb-4 mb-8">
+            <h2 className="mr-3 text-3xl font-bold">Completed</h2>
+            <div className="circle-sm bg-success text-white">
+              <span>2</span>
+            </div>
+          </div>
+          {taskData && taskData["finished"] !== undefined ? (
+            <ul>
+              {taskData["finished"].map((task) => (
+                <li className="mb-5 text-left" key={task._id}>
+                  <div className="flex items-center">
+                    <FontAwesomeIcon
+                      icon={
+                        task.status.toLowerCase() == "finished"
+                          ? faCheckCircle
+                          : faCircle
+                      }
+                      onClick={() =>
+                        handleCircleClick(task, "unfinished", "#b30000")
+                      }
+                      className="cursor-pointer transition duration-300 ease-in-out transform hover:opacity-100 opacity-50"
+                    />
+                    <p className="font-bold ml-3">{task.title}</p>
+                  </div>
+                  <p className="ml-7 text-slate-500">{task.description}</p>
+                  <p className="ml-7 text-slate-500">{task.status}</p>
+                </li>
+              ))}
+            </ul>
+          ) : taskData && !Object.keys(taskData).includes("Finished") ? (
+            <p>bro too lazy 😭</p>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
       </div>
     </div>
   );
